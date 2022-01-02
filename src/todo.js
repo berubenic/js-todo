3326622f03a6998a5e3cd9dc372b66cac3690e4f
berubenic <berubenic@gmail.com>
2022-01-01 22:16:27 -0500

    Todo init

---
+++ src/todo.js
@@ -0,0 +1,9 @@
+export class Todo {
+  constructor(title, description, dueDate, priority, checked) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.checked = checked;
+  }
+}
